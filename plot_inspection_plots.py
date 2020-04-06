487964dd5a2e68966787927825e55c245db8c807
sklandrausis <janis.akmenskalns@gmail.com>
2020-04-06 10:30:14 +0300

    save progress

--- plot_inspection_plots.py
+++ plot_inspection_plots.py
@@ -75,6 +75,45 @@ def plot_bandpass2(bandpass_frequency, time, bandpass_value, station_names, rows
     f2.savefig("results_" + s_id + "/" + "bandpass_yy.png")
 
 
+def plot_bandpass3(bandpass_frequency, time, bandpass_value, station_names, rows, columns, s_id):
+    time = Time(time/60/60/24, format='mjd').value
+    a = max(bandpass_frequency) - 10
+    b = min(time) + 10
+    xx, yy = np.meshgrid(Time(time / 60 / 60 / 24, format='mjd').value, bandpass_frequency)
+
+    f1 = plt.figure(figsize=(50, 50))
+    for stations_name in station_names:
+        time_points = len(bandpass_value[:, :, station_names.index(stations_name), 0])
+        b_x = sum(bandpass_value[:, :, station_names.index(stations_name), 0]) / time_points
+        plt.subplot(rows, columns, station_names.index(stations_name) + 1)
+        z1 = bandpass_value[:, :, station_names.index(stations_name), 0]
+        z1_new = z1.transpose() - b_x[:, None]
+        plt.text(b, a, stations_name.decode('UTF-8'), size='large', color="r")
+        c1 = plt.contourf(xx, yy, z1_new)
+        plt.xlabel("time [MJD]")
+        plt.ylabel("frequency [MHz]")
+
+    f1.colorbar(c1, cax=f1.add_axes([0.935, 0.155, 0.05, 0.7]))
+    f1.tight_layout()
+    f1.savefig("results_" + s_id + "/" + "bandpass3_xx.png")
+
+    f2 = plt.figure(figsize=(50, 50))
+    for stations_name in station_names:
+        time_points = len(bandpass_value[:, :, station_names.index(stations_name), 1])
+        b_y = sum(bandpass_value[:, :, station_names.index(stations_name), 1]) / time_points
+        plt.subplot(rows, columns, station_names.index(stations_name) + 1)
+        z2 = bandpass_value[:, :, station_names.index(stations_name), 1]
+        z2_new = z2.transpose() - b_y[:, None]
+        plt.text(b, a, stations_name.decode('UTF-8'), size='large', color="r")
+        c2 = plt.contourf(xx, yy, z2_new)
+        plt.xlabel("time [MJD]")
+        plt.ylabel("frequency [MHz]")
+
+    f2.colorbar(c2, cax=f2.add_axes([0.935, 0.155, 0.05, 0.7]))
+    f2.tight_layout()
+    f2.savefig("results_" + s_id + "/" + "bandpass3_yy.png")
+
+
 def main(solution_file_dir):
     solution_files = [SolutionFileInfo(solution_file_dir + "/" + s) for s in os.listdir(solution_file_dir)]
     max_columns = 8
@@ -83,6 +122,7 @@ def main(solution_file_dir):
         run_process(create_results_directory, sas_id)
         run_process(plot_bandpass, frequency, value, stations_names, rows, columns, sas_id)
         run_process(plot_bandpass2, frequency, time, value, stations_names, rows, columns, sas_id)
+        run_process(plot_bandpass3, frequency, time, value, stations_names, rows, columns, sas_id)
 
     for solution_file in solution_files:
         sas_id = solution_file.get_sas_id
