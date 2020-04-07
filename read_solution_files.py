1ae6edd1a844f2c8ffb1d511c4b940c962c7300e
sklandrausis <janis.akmenskalns@gmail.com>
2020-04-07 10:32:55 +0300

    save progress

--- read_solution_files.py
+++ read_solution_files.py
@@ -3,6 +3,9 @@ import os
 import argparse
 import h5py
 from astropy.time import Time
+import astropy.units as u
+from astroplan import Observer
+from astropy.coordinates import EarthLocation
 
 
 def parse_arguments():
@@ -31,30 +34,30 @@ class SolutionFileInfo:
 
     @property
     def get_stations(self):
-        return self.get_data["antenna"].value
+        return self.get_data["antenna"][()]
 
     @property
     def get_bandpass(self):
         bandpass = self.get_data["bandpass"]
-        bandpass_frequency = bandpass["freq"].value / 1e+6
-        bandpass_value = bandpass["val"].value
+        bandpass_frequency = bandpass["freq"][()] / 1e+6
+        bandpass_value = bandpass["val"][()]
         return {"bandpass": bandpass, "frequency": bandpass_frequency, "value": bandpass_value}
 
     @property
     def get_time(self):
-        return self.get_bandpass["bandpass"]["time"].value
+        return self.get_bandpass["bandpass"]["time"][()]
 
     @property
     def get_weight(self):
-        return self.get_bandpass["bandpass"]["weight"].value
+        return self.get_bandpass["bandpass"]["weight"][()]
 
     @property
     def get_start_time(self):
-        return str(Time(self.get_time[0]/60/60/24, format='mjd').to_datetime())
+        return Time(self.get_time[0]/60/60/24, format='mjd')
 
     @property
     def get_end_time(self):
-        return str(Time(self.get_time[-1]/60/60/24, format='mjd').to_datetime())
+        return Time(self.get_time[-1]/60/60/24, format='mjd')
 
     @property
     def get_duration(self):
@@ -66,10 +69,28 @@ def main(solution_file_dir):
     solution_files = [SolutionFileInfo(solution_file_dir + "/" + s) for s in os.listdir(solution_file_dir)]
 
     for solution_file in solution_files:
+        day_type = ""
+        CS001LBA_coordinates = solution_file.get_stations[0][1]
+        location = EarthLocation.from_geocentric(CS001LBA_coordinates[0]*u.m, CS001LBA_coordinates[1]*u.m, CS001LBA_coordinates[2]*u.m)
+        CS001LBA = Observer(location=location, name="CS001LBA", timezone="UTC")
+
+        if CS001LBA.is_night(solution_file.get_start_time):
+            day_type = "night"
+
+        elif solution_file.get_start_time < CS001LBA.sun_rise_time(solution_file.get_start_time, which='next'):
+            day_type = "dawn"
+
+        elif solution_file.get_start_time > CS001LBA.sun_set_time(solution_file.get_start_time, which='next'):
+            day_type = "dusk"
+
+        else:
+            day_type = "day"
+
         print("SAS ID: ", solution_file.get_sas_id,
-              "Start time: ", solution_file.get_start_time,
-              "End time: ", solution_file.get_start_time,
-              "Duration: ", solution_file.get_duration)
+              "Start time: ", solution_file.get_start_time.to_datetime(),
+              "End time: ", solution_file.get_start_time.to_datetime(),
+              "Duration: ", solution_file.get_duration, "Seconds",
+              "Day type: ", day_type)
     sys.exit(0)
 
 
